5ad4d561ed8598830c992fd672a8a8b1a73dab94
Chris Shepherd <thesheps@gmail.com>
2015-09-14 21:10:54 +0100

    Fixing build.bat to reference new nuspecs

--- src/build.bat
+++ src/build.bat
@@ -37,8 +37,10 @@ mkdir Build
 mkdir Build\lib
 mkdir Build\lib\net40
 call %nuget% pack "src\Lemonade.nuspec" -IncludeReferencedProjects -NoPackageAnalysis -verbosity detailed -o Build -Version %version% -p Configuration="%config%"
+call %nuget% pack "src\Lemonade.Data.nuspec" -IncludeReferencedProjects -NoPackageAnalysis -verbosity detailed -o Build -Version %version% -p Configuration="%config%"
 call %nuget% pack "src\Lemonade.Sql.nuspec" -IncludeReferencedProjects -NoPackageAnalysis -verbosity detailed -o Build -Version %version% -p Configuration="%config%"
 call %nuget% pack "src\Lemonade.Web.nuspec" -IncludeReferencedProjects -NoPackageAnalysis -verbosity detailed -o Build -Version %version% -p Configuration="%config%"
+call %nuget% pack "src\Lemonade.Web.Contracts.nuspec" -IncludeReferencedProjects -NoPackageAnalysis -verbosity detailed -o Build -Version %version% -p Configuration="%config%"
 
 :success
 exit 0
