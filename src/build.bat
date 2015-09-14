fc4d2860284685ef39164f0a8274ef24e31f5297
shepc <chris.shepherd@ao.com>
2015-09-14 09:58:44 +0100

    Unit tests in build.bat

--- src/build.bat
+++ src/build.bat
@@ -14,18 +14,31 @@ if "%nuget%" == "" (
 	set nuget=nuget
 )
 
+REM Restore
 call %NuGet% restore src\Lemonade\packages.config -OutputDirectory %cd%\packages -NonInteractive
 call %NuGet% restore src\Lemonade.Sql\packages.config -OutputDirectory %cd%\packages -NonInteractive
 call %NuGet% restore src\Lemonade.Web\packages.config -OutputDirectory %cd%\packages -NonInteractive
 call %NuGet% restore tests\Lemonade.Web.Tests\packages.config -OutputDirectory %cd%\packages -NonInteractive
 call %NuGet% restore tests\Lemonade.Sql.Tests\packages.config -OutputDirectory %cd%\packages -NonInteractive
 
+REM Build
 msbuild Lemonade.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=diag /nr:false
+if not "%errorlevel%"=="0" goto failure
 
+REM Unit Tests
+%GallioEcho% YourSolution\ProjectA.Tests\bin\%config%\ProjectA.Tests.dll
+if not "%errorlevel%"=="0" goto failure
+
+REM Package
 mkdir Build
 mkdir Build\lib
 mkdir Build\lib\net40
+%nuget% pack "src\Lemonade.nuspec" -IncludeReferencedProjects -NoPackageAnalysis -verbosity detailed -o Build -Version %version% -p Configuration="%config%"
+%nuget% pack "src\Lemonade.Sql.nuspec" -IncludeReferencedProjects -NoPackageAnalysis -verbosity detailed -o Build -Version %version% -p Configuration="%config%"
+%nuget% pack "src\Lemonade.Web.nuspec" -IncludeReferencedProjects -NoPackageAnalysis -verbosity detailed -o Build -Version %version% -p Configuration="%config%"
 
-%nuget% pack "src\Lemonade.nuspec" -IncludeReferencedProject -NoPackageAnalysis -verbosity detailed -o Build -Version %version% -p Configuration="%config%"
-%nuget% pack "src\Lemonade.Sql.nuspec" -IncludeReferencedProject -NoPackageAnalysis -verbosity detailed -o Build -Version %version% -p Configuration="%config%"
-%nuget% pack "src\Lemonade.Web.nuspec" -IncludeReferencedProject -NoPackageAnalysis -verbosity detailed -o Build -Version %version% -p Configuration="%config%"
+:success
+exit 0
+
+:failure
+exit -1
