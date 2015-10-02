cd2f9210450986a1df76a9144c980af77133e053
shepc <chris.shepherd@ao.com>
2015-10-02 08:43:57 +0100

    Trying to fix resource locations in Nuspec.

---
+++ src/ConsoleApplication1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Lemonade;
+using Lemonade.Resolvers;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (Feature.Switches["MyNewFeature1"])
+            {
+                Console.WriteLine("Hello World");
+            }
+        }
+    }
+}
