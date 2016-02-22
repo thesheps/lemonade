4dcacb278ed0dce12b6eb40b18ac2a559880f129
shepc <chris.shepherd@ao.com>
2016-02-22 16:25:41 +0000

    Initial angular materials and new routing

--- src/Lemonade.Web/scripts/lemonade/app.js
+++ src/Lemonade.Web/scripts/lemonade/app.js
@@ -1,29 +1,31 @@
-﻿var app = angular.module("lemonade", ["ngAnimate", "xeditable", "ngRoute"]);
+﻿var app = angular.module("lemonade", ["ngAnimate", "ngMaterial", "ngNewRouter", "lemonade.featureController"]);
 
 app.value("signalRServer", "");
 
-app.run(function(editableOptions, editableThemes) {
-    editableOptions.theme = 'bs3'; 
-    editableThemes.bs3.inputClass = 'input-sm';
-    editableThemes.bs3.buttonsClass = 'btn-xs';
-});
+app.controller("AppController", ["$router", AppController]);
 
-app.config(function($routeProvider) {
-    $routeProvider
-        .when("/Applications", {
-            templateUrl: "/Views/Applications.html",
-            controller: "applicationController"
-        })
-        .when("/Features", {
-            templateUrl: "/Views/Features.html",
-            controller: "featureController"
-        })
-        .when("/Configurations", {
-            templateUrl: "/Views/Configurations.html",
-            controller: "configurationController"
-        })
-        .when("/About", {
-            templateUrl: "/Views/About.html",
-            controller: "aboutController"
-        });
-});
+function AppController($router) {
+    $router.config([
+      { path: '/features', component: 'feature' }
+    ]);
+}
+
+//app.config(function($routeProvider) {
+//    $routeProvider
+//        .when("/Applications", {
+//            templateUrl: "/Views/Applications.html",
+//            controller: "applicationController"
+//        })
+//        .when("/Features", {
+//            templateUrl: "/Views/Features.html",
+//            controller: "featureController"
+//        })
+//        .when("/Configurations", {
+//            templateUrl: "/Views/Configurations.html",
+//            controller: "configurationController"
+//        })
+//        .when("/About", {
+//            templateUrl: "/Views/About.html",
+//            controller: "aboutController"
+//        });
+//});
