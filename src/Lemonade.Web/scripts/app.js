2379ed827cc5697c239c00a6daa8c74a34b1ad23
Chris Shepherd <thesheps@gmail.com>
2016-02-28 22:21:26 +0000

    Moving default styles into nuget package

--- src/Lemonade.Web/scripts/app.js
+++ src/Lemonade.Web/scripts/app.js
@@ -27,6 +27,28 @@ app.config(function ($routeProvider, $mdThemingProvider) {
             templateUrl: "/views/about.html",
             controller: "aboutController"
         });
+
+    $mdThemingProvider
+        .definePalette("green", {
+            '50': "#bde383",
+            '100': "#b2df6e",
+            '200': "#a7da59",
+            '300': "#9cd644",
+            '400': "#91d130",
+            '500': "#83bd2a",
+            '600': "#75a825",
+            '700': "#669321",
+            '800': "#587e1c",
+            '900': "#496a17",
+            'A100': "#c8e898",
+            'A200': "#d3edad",
+            'A400': "#dff1c2",
+            'A700': "#3b5513",
+            'contrastDefaultColor': "light"
+        });
+
+    $mdThemingProvider.theme("default")
+        .primaryPalette("green");
 });
 
 app.run(function ($rootScope) {
