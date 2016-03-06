db15c0ddcc9cebc37d15355da6a4464be057b8e9
Chris Shepherd <thesheps@gmail.com>
2016-03-06 15:41:47 +0000

    Polly

--- src/Lemonade/Services/CacheProvider.cs
+++ src/Lemonade/Services/CacheProvider.cs
@@ -1,30 +1,47 @@
 using System;
 using System.Collections.Generic;
 using Lemonade.Core.Services;
+using Polly;
 
 namespace Lemonade.Services
 {
     public class CacheProvider : ICacheProvider
     {
-        public CacheProvider(double? cacheExpiration)
+        public CacheProvider(double? cacheExpiration, int maximumRetries = 3)
         {
             _cacheExpiration = cacheExpiration;
+            _maximumRetries = maximumRetries;
         }
 
         public T GetValue<T>(string key, Func<T> strategy)
         {
             Tuple<DateTime, object> value;
             if (!_values.TryGetValue(key, out value))
-                value = new Tuple<DateTime, object>(DateTime.Now, strategy());
+                value = new Tuple<DateTime, object>(DateTime.Now, GetValue(strategy));
             else if (value.Item1.AddMinutes(_cacheExpiration.GetValueOrDefault()) <= DateTime.Now)
-                value = new Tuple<DateTime, object>(DateTime.Now, strategy());
+                value = new Tuple<DateTime, object>(DateTime.Now, GetValue(strategy));
 
             _values[key] = value;
 
             return (T)value.Item2;
         }
 
+        private T GetValue<T>(Func<T> strategy)
+        {
+            var result = default(T);
+
+            var exception = Policy.Handle<Exception>()
+                .Retry(_maximumRetries)
+                .ExecuteAndCapture(() => result = strategy())
+                .FinalException;
+
+            if (exception != null) throw exception;
+
+            return result;
+        }
+
         private readonly Dictionary<string, Tuple<DateTime, object>> _values = new Dictionary<string, Tuple<DateTime, object>>();
         private readonly double? _cacheExpiration;
+        private readonly int _maximumRetries;
     }
 }
--- tests/Lemonade.Tests/GivenCacheProvider.cs
+++ tests/Lemonade.Tests/GivenCacheProvider.cs
@@ -39,7 +39,7 @@ namespace Lemonade.Tests
         }
 
         [Test]
-        public void WhenTheCacheExpirationPeriodHasExpired_ThenTheValueIsRetrievedUsingTheStrategt()
+        public void WhenTheCacheExpirationPeriodHasExpired_ThenTheValueIsRetrievedUsingTheStrategy()
         {
             var strategyIsUsed = false;
             var cacheProvider = new CacheProvider(0.1);
@@ -55,6 +55,23 @@ namespace Lemonade.Tests
 
             Assert.That(strategyIsUsed, Is.True);
             Assert.That(value, Is.True);
+        }
+
+        [TestCase(3)]
+        [TestCase(4)]
+        [TestCase(7)]
+        public void WhenTheStrategyResultsInAnException_ThenItIsRetriedAConfigurableNumberOfTimes(int retries)
+        {
+            var attempts = 0;
+            var cacheProvider = new CacheProvider(0.1, retries);
+
+            var value = cacheProvider.GetValue<bool>("TEST", () =>
+            {
+                attempts++;
+                throw new Exception();
+            });
+
+            Assert.That(attempts, Is.EqualTo(retries + 1));
         }
     }
 }
