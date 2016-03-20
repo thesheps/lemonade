f93abe6f7bdf89b6e566fd02b6dc0a1618476847
Chris Shepherd <thesheps@gmail.com>
2016-03-20 20:39:01 +0000

    Fixing LemonadeConfigurationSection

--- src/Lemonade/LemonadeConfigurationSection.cs
+++ src/Lemonade/LemonadeConfigurationSection.cs
@@ -19,11 +19,14 @@ namespace Lemonade
         [ConfigurationProperty("CacheProvider", IsRequired = false)]
         public string CacheProvider => this["CacheProvider"] as string;
 
+        [ConfigurationProperty("CacheExpiration", IsRequired = false)]
+        public double? CacheExpiration => this["CacheExpiration"] as double? ?? 0;
+
         [ConfigurationProperty("ApplicationName", IsRequired = false)]
         public string ApplicationName => this["ApplicationName"] as string ?? AppDomain.CurrentDomain.FriendlyName.Replace(".exe", string.Empty);
 
-        [ConfigurationProperty("CacheExpiration", IsRequired = false)]
-        public double? CacheExpiration => this["CacheExpiration"] as double? ?? 0;
+        [ConfigurationProperty("RetryPolicy", IsRequired = false)]
+        public string RetryPolicy => this["RetryPolicy"] as string;
 
         [ConfigurationProperty("MaximumAttempts", IsRequired = false)]
         public int? MaximumAttempts => this["MaximumAttempts"] as int? ?? 3;
